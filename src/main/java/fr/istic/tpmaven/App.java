e10527d238cb6cfe12f5cbc7083061bf0dce6185
Quentin Guillou <guillou.quentin.29@gmail.com>
2016-09-07 10:45:12 +0200

    Tp maven

---
+++ src/main/java/fr/istic/tpmaven/App.java
@@ -0,0 +1,22 @@
+package fr.istic.tpmaven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+	private App() {	}
+
+    /**
+     *
+     * @param args
+     */
+	public static void main(final String[] args) {
+        System.out.println("Hello World!");
+    }
+	
+	/**
+	 * 
+	 */
+	public void test() { }
+}
